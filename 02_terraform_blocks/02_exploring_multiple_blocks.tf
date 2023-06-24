24d4c2b74b44a5855396abe2f6800fc66a69bb2d
Shubham Gour <iamtheshubhamgour@gmail.com>
2023-06-24 14:05:37 +0530

    Add the multiple block file

---
+++ 02_terraform_blocks/02_exploring_multiple_blocks.tf
@@ -0,0 +1,9 @@
+output "first-block" {
+  value = "Dataone from the block one"
+}
+output "second-block" {
+  value = "Data two from block two"
+}
+output "third-block" {
+  value = "Data three from block three"
+}
